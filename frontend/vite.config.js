ad09180fbe0606d6f1ba5f09cdf0c1d4d1965e27
richzltw <120192959+richzltw@users.noreply.github.com>
2026-02-07 09:54:43 +0800

    4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    host: true,
+    allowedHosts: true // 或者指定具体域名 ['sublime-quietude-production-0477.up.railway.app']
+  },  
   server: {
     port: 5173,
     proxy: {
