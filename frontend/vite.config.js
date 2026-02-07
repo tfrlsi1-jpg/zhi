17ca9c5a27c6632195624c0ef9ff5f557b2642e3
richzltw <120192959+richzltw@users.noreply.github.com>
2026-02-07 11:37:42 +0800

    6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 8080,
-    host: true,
-    allowedHosts: true // 或者指定具体域名 ['sublime-quietude-production-0477.up.railway.app']
-  },  
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: env.VITE_API_URL || 'http://localhost:3001',
-        changeOrigin: true,
+export default defineConfig(({ mode }) =>{
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return{
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3001',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  preview: {
-    port: 8080,
-    host: true,
-    proxy: {
-      '/api': {
-        target: env.VITE_API_URL || 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  }
+    preview: {
+        port: 8080,        // 告訴 Railway 監聽哪個 Port
+        host: true,        // 允許公網訪問
+        allowedHosts: true, // 防止域名被擋掉
+        proxy: {           // 轉發 API 請求到後端
+          '/api': {
+            target: env.VITE_API_URL || 'http://localhost:3001',
+            changeOrigin: true,
+          },
+        },
+    }    
+  };
 });
